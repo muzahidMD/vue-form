43954ac04a4047efd25b77a1ae7a7c0db375a1ad
Muzahidul Islam <mujahidul.core@gmail.com>
2025-03-28 02:45:02 +0600

    episode-02

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <script setup>
-import { ref } from 'vue';
-
-const color = ref('None');
-
-function getStyle() {
-  return {
-    backgroundColor: color.value
-  }
-}
+import { reactive, ref } from 'vue';
+const person = reactive({
+  name: "Muzahidul Islam",
+  age: 25,
+  job: "web developer",
+})
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="pb-5">Your favorite color: {{ color }} </p>
-    <p>Your favorite color:
-      <input class="p-5 border" v-model="color">
-    <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
-    </p>
+    <p class="mb-10">{{ person }} </p>
+
+    <!-- <div class="w-32 h-32 mt-10">
+      <label for="name">Name</label>
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person.name">
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person.age">
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person.job">
+    </div> -->
+    <div class="w-32 h-32 mt-10" v-for="(value, key, index) in person" :key="index">
+      <label for="name">{{ key }}</label>
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]">
+    </div>
+
 
   </section>
 </template>
